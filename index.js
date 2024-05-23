65bb2e1ba4b7d0f60b59682ad6f411b0f1476336
THASLEEMA-KM <thasleemakm5@gmail.com>
2024-05-24 00:49:57 +0530

    Restaurant finding app server created an completed

---
+++ index.js
@@ -0,0 +1,22 @@
+// import json-server
+const jsonServer = require('json-server')
+
+// create server for media player
+const MPServer = jsonServer.create()
+
+// create middleware used by JSON server
+const middleware = jsonServer.defaults()
+
+// set up route fro json file in server
+const route = jsonServer.router('restaurants.json')
+
+// set up pot for running server app
+const PORT = 3000 || process.env.PORT
+
+
+MPServer.use(middleware)
+MPServer.use(route)
+MPServer.listen(PORT,()=>
+{
+    console.log(`Restaurant Server Startedat Port ${PORT} and waiting for client Request`);
+})
